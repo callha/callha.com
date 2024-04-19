ad86be3d2df04a26fa7d7b444e5c4a2fb3aac5a8
qinjiangban <tanjiangban@gmail.com>
2024-04-19 18:02:51 +0800

    ghpages

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  webpack: config => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding',)
-    return config
-  }
+  output: 'export'
 }
 
 export default nextConfig;
