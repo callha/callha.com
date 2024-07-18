c40c4213b287adcf30f58aa5d8459ebad5ab6fa3
Qin <tanjiangban@gmail.com>
2024-07-19 01:07:02 +0800

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,7 +4,7 @@
 
 import { ThemeProvider } from "next-themes";
 
-export default function Providers({ children }) {
+export default function Providers({ children }:any) {
   return (
     <div>
       <ThemeProvider>{children}</ThemeProvider>
